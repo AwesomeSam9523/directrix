5dff2d8c58c52fe4b1dcde5538f01e89c692eb8f
AwesomeSam9523 <samakshgupta04@gmail.com>
2021-06-25 23:11:56 +0530

    made create account pending data file saving

--- main.py
+++ main.py
@@ -2,10 +2,16 @@
 # Refer README.md for features and contributions
 
 #create acc- random.randint(10**9, 10**10)
-import time, json
+import random
+import time, json, re
 from tkinter import *
+from tkinter import messagebox
 from PIL import Image, ImageFont, ImageDraw, ImageTk
-import csv
+from functools import partial
+
+namecheck = re.compile('^[a-z]+$', re.IGNORECASE)
+pancheck = re.compile('^[a-z0-9]+$', re.IGNORECASE)
+pswdcheck = re.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$")
 
 def x(value):
     return int((root.wm_maxsize()[0]/1536)*value)
@@ -40,6 +46,79 @@ def create_login():
     if not found:
         status.configure(text="Username doesn't exist!", fg="#FF0000")
 
+def cancelcreate(*args):
+    login_field.place(x=x(95), y=y(350))
+    login_field.focus_set()
+    password_field.place(x=x(95), y=y(490))
+    submit_login.place(x=x(85), y=y(600))
+    submit_create.place(x=x(300), y=y(600))
+    status.place(x=x(85), y=y(562))
+    for i in args:
+        i.place_forget()
+
+    dashbg = Image.open("data/images/bg.png")
+    dashbg = dashbg.resize(root.wm_maxsize())
+    root.bgimg = ImageTk.PhotoImage(dashbg)
+    bg_canvas.itemconfig(root.rootbgimage, image=root.bgimg)
+
+def finalsubmit(fname, lname, email, age, pan, gender, aadhar, add1, add2, pswd, cpswd):
+    g_fname = fname.get().replace(" ", "")
+    g_lname = lname.get().replace(" ", "")
+    g_email = email.get().replace(" ", "")
+    g_age = age.get().replace(" ", "")
+    g_pan = pan.get().replace(" ", "")
+    g_gender = gender.get()
+    g_aadhar = aadhar.get().replace(" ", "")
+    g_add1 = add1.get()
+    g_add2 = add2.get()
+    g_pswd = pswd.get()
+    g_cpswd = cpswd.get()
+
+    if any([g_fname == "", g_lname == "", g_email == "", g_age == "", g_pan == "", g_gender == "", g_aadhar == "",
+            g_add1 == "", g_add2 == "", g_pswd == "", g_cpswd == ""]):
+        create_popup("Please make sure all the fields are filled!")
+        return
+
+    if (not namecheck.search(g_fname)) or (not namecheck.search(g_lname)):
+        create_popup("Invalid Name! Use of only alphabets is allowed")
+        return
+
+    if ("@" not in g_email) or ("." not in g_email):
+        create_popup("Invalid mail id! Please re-check")
+        return
+
+    try: g_age = int(g_age)
+    except:
+        create_popup("Age must be numeric only!")
+        return
+
+    if (len(g_pan) != 10) or (not pancheck.search(g_pan)):
+        create_popup("Invalid PAN number!")
+        return
+
+    if len(g_aadhar) != 12:
+        create_popup("Invalid Aadhar number!")
+    try: g_aadhar = int(g_aadhar)
+    except:
+        create_popup("Invalid Aadhar number!")
+        return
+
+    if not pswdcheck.search(g_pswd):
+        create_popup("Password should meet following criteria:\n"
+                     "- Minimum 8 characters\n"
+                     "- Must contain 1 uppercase letter\n"
+                     "- Must contain 1 lowercase letter\n"
+                     "- Must contain 1 symbol\n"
+                     "- Must contain 1 digit")
+        return
+
+    if g_cpswd != g_pswd:
+        create_popup("Passwords do not match!")
+        return
+
+def create_popup(text):
+    messagebox.showwarning("Error", text)
+
 def submit_data():
     login_field.place_forget()
     password_field.place_forget()
@@ -62,8 +141,13 @@ def submit_data():
     age.place(x=x(1060), y=y(238))
     pan = Entry(root, width=x(25), font=("Helvetica", 22))
     pan.place(x=x(400), y=y(312))
-    gender = Entry(root, width=x(5), font=("Helvetica", 22))
-    gender.place(x=x(1060), y=y(312))
+    gender = StringVar()
+    male = Radiobutton(root, text="Male", variable=gender, value="M", font=("Helvetica", 20), bg="#ffffff")
+    male.place(x=x(1060), y=y(305))
+    female = Radiobutton(root, text="Female", variable=gender, value="F", font=("Helvetica", 20), bg="#ffffff")
+    female.place(x=x(1160), y=y(305))
+    other = Radiobutton(root, text="Other", variable=gender, value="O", font=("Helvetica", 20), bg="#ffffff")
+    other.place(x=x(1290), y=y(305))
     aadhar = Entry(root, width=x(25), font=("Helvetica", 22))
     aadhar.place(x=x(400), y=y(386))
     add1 = Entry(root, width=x(30), font=("Helvetica", 22))
@@ -79,6 +163,9 @@ def submit_data():
     final.place(x=x(1030), y=y(690))
     cancel = Button(root, text="Cancel", font=("Arial", 22), width=x(11), bg="#FF4E3F")
     cancel.place(x=x(1290), y=y(690))
+    cancel.configure(command=partial(cancelcreate, fname, lname, email, age, pan, male, female, other, aadhar, add1, add2, pswd,
+                        cpswd, final, cancel))
+    final.configure(command=partial(finalsubmit, fname, lname, email, age, pan, gender, aadhar, add1, add2, pswd, cpswd))
     return
     usen = input('Pls enter a username: ')
     pas = input('Pls enter a password: ')
@@ -90,7 +177,8 @@ def submit_data():
     pan= input('Pls enter your pan number: ')
     acn = input('Pls enter your aadhar card number: ')
 
-    root.accountsdata["123"] = {"password":pas, "email":email, "address":add, "age":age, "Gender":gn, "MOBILE NUMBER": mno, "PAN NO.":pan, "AADHAR CARD NO.":acn}
+    userid = random.randint(10**10, 10**11)
+    root.accountsdata[str(userid)] = {"password":pas, "email":email, "address":add, "age":age, "Gender":gn, "MOBILE NUMBER": mno, "PAN NO.":pan, "AADHAR CARD NO.":acn}
     with open('data/accounts.json', 'w') as file:
         file.write(root.accountsdata)
 
@@ -146,11 +234,9 @@ root.bgimg = ImageTk.PhotoImage(bg)
 bg_canvas = Canvas(root, width=root.x, height=root.wm_maxsize()[1])
 bg_canvas.place(x=x(0), y=y(0))
 root.rootbgimage = bg_canvas.create_image(0, 0, image=root.bgimg, anchor="nw")
-print(root.rootbgimage, type(root.rootbgimage))
 
 mainbg = Frame(root, width=root.wm_maxsize()[0], height=25)
 mainbg.place(x=x(0), y=y(0))
-
 close = Button(mainbg, text="Close", command=root.destroy)
 close.place(x=(root.x- x(42)), y=y(2))
 restore = Button(mainbg, text="Restore", command=restore_root)
@@ -181,8 +267,6 @@ with open("data/data.json", "r") as f:
 def withdraw():
     userid = 8567375658
     var = root.data.get(str(userid))
-    print(root.data)
-    print(var)
     balance = var.get('balance')
     withd = int(input('Please enter the amount you want to withdraw = '))
     if withd > balance:
@@ -200,8 +284,6 @@ def savecode():
 def deposit():
     userid = 8567375658
     var = root.data.get(str(userid))
-    print(root.data)
-    print(var)
     balance = var.get('balance')
     dep = int(input("AMOUNT"))
     alloted = balance*(.25)
@@ -220,8 +302,6 @@ with open('data/data.json', 'w') as file:
 def loan():
     userid = 8567375658
     var = root.data.get(str(userid))
-    print(root.data)
-    print(var)
     balance = var.get('balance')
     exp = int(input("AMOUNT EXPECTED:"))
     inc = int(input("ANNUAL INCOME:"))
@@ -241,6 +321,5 @@ def loan():
         print("NOT CONFIRMED BECAUSE THE AMOUNT EXPECTED IS TOO LARGE")
 
 
-withdraw()
 root.mainloop()
 
