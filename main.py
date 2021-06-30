0450aeb991f91c16bd75f80f9b6e7f06f5d92236
Samaksh Gupta <samakshgupta04@gmail.com>
2021-06-30 13:26:21 +0530

    dashboard complete. Added memory caching

--- main.py
+++ main.py
@@ -29,7 +29,6 @@ def savedata():
     with open('data/statements.json', 'w') as st:
         st.write(json.dumps(root.statements, indent=2))
 
-
 def create_login():
     username = login_field.get()
     password = password_field.get()
@@ -58,7 +57,7 @@ def create_login():
     if not found:
         status.configure(text="Username doesn't exist!", fg="#FF0000")
 
-def cancelcreate(*args):
+def showhomepage(*args):
     login_field.place(x=x(95), y=y(350))
     login_field.focus_set()
     password_field.place(x=x(95), y=y(490))
@@ -71,6 +70,16 @@ def cancelcreate(*args):
     dashbg = dashbg.resize(root.wm_maxsize())
     root.bgimg = ImageTk.PhotoImage(dashbg)
     bg_canvas.itemconfig(root.rootbgimage, image=root.bgimg)
+
+    password_field.delete(0, END)
+    login_field.delete(0, END)
+    cacheid = root.cache.get("id")
+    if cacheid is not None:
+        root.cache["skip"] = False
+        cacheupdate()
+        login_field.insert(0, cacheid)
+        password_field.focus_set()
+    else: login_field.focus_set()
 
 def finalsubmit(fname, lname, email, age, pan, gender, aadhar, add1, add2, pswd, cpswd, **kwargs):
     g_fname = fname.get().replace(" ", "")
@@ -109,6 +118,8 @@ def finalsubmit(fname, lname, email, age, pan, gender, aadhar, add1, add2, pswd,
 
     if len(g_aadhar) != 12:
         create_popup("Invalid Aadhar number!")
+        return
+
     try: g_aadhar = int(g_aadhar)
     except:
         create_popup("Invalid Aadhar number!")
@@ -129,7 +140,7 @@ def finalsubmit(fname, lname, email, age, pan, gender, aadhar, add1, add2, pswd,
 
     userid = random.randint(10 ** 10, 10 ** 11)
     root.accountsdata[str(userid)] = {"pswd": g_pswd, "email": g_email, "address": f"{g_add1}, {g_add2}", "age": g_age, "gender": g_gender,
-                                      "pan": g_pan, "aadhar": g_aadhar, "name":f"{g_fname} {g_lname}"}
+                                      "pan": g_pan, "aadhar": g_aadhar, "name":f"{g_fname} {g_lname}", "id":str(userid)}
     root.data[str(userid)] = {"balance":0}
     savedata()
     dashboard(root.accountsdata[str(userid)], kwargs["ele"])
@@ -182,8 +193,8 @@ def submit_data():
     final.place(x=x(1030), y=y(690))
     cancel = Button(root, text="Cancel", font=("Arial", 22), width=x(11), bg="#FF4E3F")
     cancel.place(x=x(1290), y=y(690))
-    cancel.configure(command=partial(cancelcreate, fname, lname, email, age, pan, male, female, other, aadhar, add1, add2, pswd,
-                        cpswd, final, cancel))
+    cancel.configure(command=partial(showhomepage, fname, lname, email, age, pan, male, female, other, aadhar, add1, add2, pswd,
+                                     cpswd, final, cancel))
     final.configure(command=partial(finalsubmit, fname, lname, email, age, pan, gender, aadhar, add1, add2, pswd, cpswd,
                                     ele=(fname, lname, email, age, pan, male, female, other, aadhar, add1, add2, pswd, cpswd, final, cancel)))
 
@@ -192,19 +203,24 @@ def restore_root():
         root.attributes('-fullscreen', False)
         root.fs = False
         restore["text"] = "Maximize"
-        restore.place(x=x(root.x - x(110)), y=y(2))
-        close.place(x=x(root.x - x(47)), y=y(2))
+        restore.place(x=(root.x - x(115)), y=y(2))
+        close.place(x=(root.x - x(47)), y=y(2))
+        root.cache["fs"] = False
     else:
         root.attributes('-fullscreen', True)
         root.fs = True
         restore["text"] = "Restore"
-        restore.place(x=x(root.x - x(93)), y=y(2))
-        close.place(x=x(root.x - x(42)), y=y(2))
+        restore.place(x=(root.x - x(95)), y=y(2))
+        close.place(x=(root.x - x(42)), y=y(2))
+        root.cache["fs"] = True
+    cacheupdate()
 
 def dashboard(data, *args):
     for i in args[0]:
         i.place_forget()
-
+    root.cache["id"] = data["id"]
+    root.cache["skip"] = True
+    cacheupdate()
     dashbg = Image.open("data/images/dashboard.png")
     dashbg = dashbg.resize(root.wm_maxsize())
     root.bgimg = ImageTk.PhotoImage(dashbg)
@@ -224,103 +240,58 @@ def dashboard(data, *args):
     welcome = Label(topframe, text=f"Welcome, {data['name']}!", font=("Arial", 20), bg="#B576FF")
     welcome.place(x=x(85), y=(20))
 
-    depimg = Image.open("data/images/deposit.PNG").resize((x(100), y(100)))
+    yourdash = Label(root, text=f"Your Dashboard", font=("Arial", x(35), "bold"), bg="white")
+    yourdash.place(x=x(595), y=(160))
+
+    depimg = Image.open("data/images/deposit.PNG").resize((x(150), y(150)))
     depimg = ImageTk.PhotoImage(depimg)
-    depositbtn = Button(root, text="Deposit", compound=TOP, image=depimg, font=("Arial", 13, "bold"))
-    depositbtn.place(x=x(800), y=y(300))
+    depositbtn = Button(root, text="Deposit", compound=TOP, image=depimg, font=("Arial", 13, "bold"), bg="white")
+    depositbtn.place(x=x(400), y=y(300))
     depositbtn.image = depimg
 
-    withimg = Image.open("data/images/withdraw.png").resize((x(100), y(100)))
+    withimg = Image.open("data/images/withdraw.png").resize((x(150), y(150)))
     withimg = ImageTk.PhotoImage(withimg)
-    withbtn = Button(root, text="Withdraw", compound=TOP, image=withimg, font=("Arial", 13, "bold"))
-    withbtn.place(x=x(940), y=y(300))
+    withbtn = Button(root, text="Withdraw", compound=TOP, image=withimg, font=("Arial", 13, "bold"), bg="white")
+    withbtn.place(x=x(600), y=y(300))
     withbtn.image = withimg
 
-    loanimg = Image.open("data/images/loan.jpg").resize((x(100), y(100)))
+    loanimg = Image.open("data/images/loan.jpg").resize((x(150), y(150)))
     loanimg = ImageTk.PhotoImage(loanimg)
-    loanbtn = Button(root, text="Loan", compound=TOP, image=loanimg, font=("Arial", 13, "bold"))
-    loanbtn.place(x=x(1080), y=y(300))
+    loanbtn = Button(root, text="Loan", compound=TOP, image=loanimg, font=("Arial", 13, "bold"), bg="white")
+    loanbtn.place(x=x(800), y=y(300))
     loanbtn.image = loanimg
 
-    statementsimg = Image.open("data/images/statements.png").resize((x(100), y(100)))
+    statementsimg = Image.open("data/images/statements.png").resize((x(150), y(150)))
     statementsimg = ImageTk.PhotoImage(statementsimg)
-    statementsbtn = Button(root, text="Statement", compound=TOP, image=statementsimg, font=("Arial", 13, "bold"))
-    statementsbtn.place(x=x(1220), y=y(300))
+    statementsbtn = Button(root, text="View Statement", compound=TOP, bg="white", image=statementsimg, font=("Arial", 13, "bold"))
+    statementsbtn.place(x=x(1000), y=y(300))
     statementsbtn.image = statementsimg
 
-    viewaccimg = Image.open("data/images/viewacc.png").resize((x(100), y(100)))
+    viewaccimg = Image.open("data/images/viewacc.png").resize((x(150), y(150)))
     viewaccimg = ImageTk.PhotoImage(viewaccimg)
-    viewaccbtn = Button(root, text="My Account", compound=TOP, image=viewaccimg, font=("Arial", 13, "bold"))
-    viewaccbtn.place(x=x(800), y=y(460))
+    viewaccbtn = Button(root, text="My Account", compound=TOP, image=viewaccimg, font=("Arial", 13, "bold"), bg="white")
+    viewaccbtn.place(x=x(400), y=y(520))
     viewaccbtn.image = viewaccimg
 
-    fdimg = Image.open("data/images/fixeddep.png").resize((x(100), y(100)))
+    fdimg = Image.open("data/images/fixeddep.png").resize((x(150), y(150)))
     fdimg = ImageTk.PhotoImage(fdimg)
-    fdbtn = Button(root, text="Fixed Dep", compound=TOP, image=fdimg, font=("Arial", 13, "bold"))
-    fdbtn.place(x=x(940), y=y(460))
+    fdbtn = Button(root, text="Fixed Deposit", compound=TOP, image=fdimg, font=("Arial", 13, "bold"), bg="white")
+    fdbtn.place(x=x(600), y=y(520))
     fdbtn.image = fdimg
 
-    chpswdimg = Image.open("data/images/passwd.png").resize((x(100), y(100)))
+    chpswdimg = Image.open("data/images/passwd.png").resize((x(150), y(150)))
     chpswdimg = ImageTk.PhotoImage(chpswdimg)
-    chpswdbtn = Button(root, text="Change Pswd", compound=TOP, image=chpswdimg, font=("Arial", 13, "bold"))
-    chpswdbtn.place(x=x(1080), y=y(460))
+    chpswdbtn = Button(root, text="Change Pswd.", compound=TOP, image=chpswdimg, font=("Arial", 13, "bold"), bg="white")
+    chpswdbtn.place(x=x(800), y=y(520))
     chpswdbtn.image = chpswdimg
 
-root = Tk()
-root.geometry(f"{root.wm_maxsize()[0]}x{root.wm_maxsize()[1]}")
-root.x = root.wm_maxsize()[0]
-root.y = root.wm_maxsize()[1]
-root.attributes('-fullscreen', True)
-root.fs = True
-bg = Image.open("data/images/bg.png")
-bg = bg.resize(root.wm_maxsize())
-root.bgimg = ImageTk.PhotoImage(bg)
-
-bg_canvas = Canvas(root, width=root.x, height=root.wm_maxsize()[1])
-bg_canvas.place(x=x(0), y=y(0))
-root.rootbgimage = bg_canvas.create_image(0, 0, image=root.bgimg, anchor="nw")
-
-mainbg = Frame(root, width=root.wm_maxsize()[0], height=25)
-mainbg.place(x=x(0), y=y(0))
-close = Button(mainbg, text="Close", command=root.destroy)
-close.place(x=(root.x- x(42)), y=y(2))
-restore = Button(mainbg, text="Restore", command=restore_root)
-restore.place(x=(root.x- x(93)), y=y(2))
-
-login_field = Entry(root, width=x(25), font=("Helvetica", 22))
-login_field.place(x=x(95), y=y(350))
-login_field.focus_set()
-
-password_field = Entry(root, width=x(25), font=("Helvetica", 22), show="*")
-password_field.place(x=x(95), y=y(490))
-
-submit_login = Button(root, text="Login", font=("Arial", 18), width=x(14), bg="#62E1FF", command=create_login)
-submit_login.place(x=x(85), y=y(600))
-
-submit_create = Button(root, text="Create Account", font=("Arial", 18), width=x(14), bg="#62E1FF", command=submit_data)
-submit_create.place(x=x(300), y=y(600))
-
-status = Label(root, font=("Helvetica", 16), bg="#ffffff", fg="#FF0000")
-
-with open("data/accounts.json", "r") as f:
-    root.accountsdata = json.load(f)
-
-with open("data/data.json", "r") as f:
-    root.data = json.load(f)
-
-with open("data/statements.json", "r") as f:
-    root.statements = json.load(f)
-
-
-def createstatements(CREDIT, balance):
-    userid = 8567375658
-    prev = root.statements.get(str(userid), [])
-    print(prev)
-
-    prev.append({"type": withdraw(), "Withdraw": nam})
-    prev.append({"type": deposit(), "Deposit": n_balance})
-    savedata()
-
+    logoutimg = Image.open("data/images/logout.png").resize((x(150), y(150)))
+    logoutimg = ImageTk.PhotoImage(logoutimg)
+    logoutbtn = Button(root, text="Logout", compound=TOP, image=logoutimg, font=("Arial", 13, "bold"), bg="white")
+    logoutbtn.place(x=x(1000), y=y(520))
+    logoutbtn.image = logoutimg
+    logoutbtn.configure(command=partial(showhomepage, userimage, welcome, yourdash, depositbtn, withbtn, loanbtn,
+                                       statementsbtn, viewaccbtn, fdbtn, chpswdbtn, logoutbtn, topframe))
 
 def withdraw():
     userid = 8567375658
@@ -369,6 +340,73 @@ def loan():
     else :
         print("NOT CONFIRMED BECAUSE THE AMOUNT EXPECTED IS TOO LARGE")
 
-dashboard(root.accountsdata["58625216057"], (login_field, password_field, submit_login, submit_create, status))
+def cacheupdate():
+    with open("data/cache.json", "w") as f:
+        f.write(json.dumps(root.cache, indent=2))
+
+def createstatements(userid, amount, t):
+    prev = root.statements.get(str(userid), [])
+    prev.append({"type": t, "amt": amount})
+    savedata()
+
+root = Tk()
+root.geometry(f"{root.wm_maxsize()[0]}x{root.wm_maxsize()[1]}")
+root.x = root.wm_maxsize()[0]
+root.y = root.wm_maxsize()[1]
+root.attributes('-fullscreen', True)
+root.fs = True
+bg = Image.open("data/images/bg.png")
+bg = bg.resize(root.wm_maxsize())
+root.bgimg = ImageTk.PhotoImage(bg)
+
+with open("data/accounts.json", "r") as f:
+    root.accountsdata = json.load(f)
+
+with open("data/data.json", "r") as f:
+    root.data = json.load(f)
+
+with open("data/statements.json", "r") as f:
+    root.statements = json.load(f)
+
+with open("data/cache.json", "r") as f:
+    root.cache = json.load(f)
+
+bg_canvas = Canvas(root, width=root.x, height=root.wm_maxsize()[1])
+bg_canvas.place(x=x(0), y=y(0))
+root.rootbgimage = bg_canvas.create_image(0, 0, image=root.bgimg, anchor="nw")
+
+mainbg = Frame(root, width=root.wm_maxsize()[0], height=25)
+mainbg.place(x=x(0), y=y(0))
+close = Button(mainbg, text="Close", command=root.destroy)
+close.place(x=(root.x- x(42)), y=y(2))
+restore = Button(mainbg, text="Restore", command=restore_root)
+restore.place(x=(root.x- x(93)), y=y(2))
+
+login_field = Entry(root, width=x(25), font=("Helvetica", 22))
+login_field.place(x=x(95), y=y(350))
+
+password_field = Entry(root, width=x(25), font=("Helvetica", 22), show="*")
+password_field.place(x=x(95), y=y(490))
+cacheid = root.cache.get("id")
+if cacheid is not None:
+    login_field.insert(0, cacheid)
+    password_field.focus_set()
+else: login_field.focus_set()
+
+submit_login = Button(root, text="Login", font=("Arial", 18), width=x(14), bg="#62E1FF", command=create_login)
+submit_login.place(x=x(85), y=y(600))
+
+submit_create = Button(root, text="Create Account", font=("Arial", 18), width=x(14), bg="#62E1FF", command=submit_data)
+submit_create.place(x=x(300), y=y(600))
+
+status = Label(root, font=("Helvetica", 16), bg="#ffffff", fg="#FF0000")
+
+fs = root.cache.setdefault("fs", True)
+if not fs: restore_root()
+cacheupdate()
+
+if root.cache.get("skip", False):
+    dashboard(root.accountsdata[str(cacheid)], (login_field, password_field, submit_login, submit_create, status))
+
 root.mainloop()
 
